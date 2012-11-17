06d41aaf323008c04ef19b57170cd205b878d162
Zach Aysan <zachaysan@gmail.com>
2012-11-17 18:24:34 -0500

    Save photos like a boss

--- qt/crawlers/popular.py
+++ qt/crawlers/popular.py
@@ -1,12 +1,14 @@
 from fhp.models.photo_stream import PhotoStream
-
+import subprocess
 p = PhotoStream()
 
-MAX = 4
+MAX = 10
 i = 0
 
 for photo in p:
     i += 1
     if i > MAX:
         break
-    print photo.image_url_size(100)
+    url = photo.image_url_size(100)
+    process_call = "wget --output-document=%s.jpg %s" % (photo.id, url)
+    subprocess.call(process_call, shell=True)
