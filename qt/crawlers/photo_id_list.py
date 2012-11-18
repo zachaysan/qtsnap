9e45900bd17073e0d5209a9fa8ad7afa6a659299
Zach Aysan <zachaysan@gmail.com>
2012-11-18 10:21:25 -0500

    :shipit:

---
+++ qt/crawlers/photo_id_list.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+from fhp.models.photo import photo
+
+with open("ids") as id_file:
+    for line in id_file.readlines():
+        try:
+            photo_id = int(line.chomp())
+        except:
+            print "skipping line"
+            continue
+        if photo_id % 100 == 0:
+            print photo_id
+        if os.path.isfile("photos/%s.jpg" % photo.id):
+            continue
+        url = photo.image_url_size(100)
+        process_call = "wget --output-document=photos/%s.jpg %s" % (photo.id, url)
+        subprocess.call(process_call, shell=True)
