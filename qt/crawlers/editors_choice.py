b458c2767d5e7f054800ff75763078e01ae663c2
Zach Aysan <zachaysan@gmail.com>
2012-11-17 20:02:08 -0500

    choice photos for choice people

---
+++ qt/crawlers/editors_choice.py
@@ -0,0 +1,19 @@
+from fhp.models.photo_stream import PhotoStream
+import subprocess
+import os
+
+p = PhotoStream("editors")
+
+MAX = (1000 * 1000)
+i = 0
+
+for photo in p:
+    if os.path.isfile("photos/%s.jpg" % photo.id):
+        continue
+    
+    i += 1
+    if i > MAX:
+        break
+    url = photo.image_url_size(100)
+    process_call = "wget --output-document=photos/%s.jpg %s" % (photo.id, url)
+    subprocess.call(process_call, shell=True)
