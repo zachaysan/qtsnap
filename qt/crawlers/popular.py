b209ab5b6b82f5ccd173f3584c863bf270f5b707
Zach Aysan <zachaysan@gmail.com>
2012-11-17 19:35:01 -0500

    more photos and no dups plzthxbia

--- qt/crawlers/popular.py
+++ qt/crawlers/popular.py
@@ -1,11 +1,16 @@
 from fhp.models.photo_stream import PhotoStream
 import subprocess
+import os
+
 p = PhotoStream()
 
-MAX = (100 * 1000)
+MAX = (1000 * 1000)
 i = 0
 
 for photo in p:
+    if os.path.isfile("photos/%s.jpg" % photo.id):
+        continue
+    
     i += 1
     if i > MAX:
         break
